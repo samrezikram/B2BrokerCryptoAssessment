7682c35e8f8100a135938a21f365908c1c785e25
samrez Ikram <samrezikram@yahoo.com>
2022-12-27 00:31:43 +0400

    Initial Commit

---
+++ B2BrokerCryptoAssessment/B2BrokerCryptoAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  B2BrokerCryptoAssessmentApp.swift
+//  B2BrokerCryptoAssessment
+//
+//  Created by Samrez Ikram on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct B2BrokerCryptoAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ B2BrokerCryptoAssessment/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  B2BrokerCryptoAssessment
+//
+//  Created by Samrez Ikram on 27/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
